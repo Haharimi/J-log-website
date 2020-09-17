b846b30d24824cc2695cb7a6b476660e71536737
haharimi <haharimi@naver.com>
2020-09-17 20:27:51 +0900

    Join 페이지 완성:
    ?
    
    q

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,23 @@
-export const join = (req, res) => res.render("join", { pageTitle: "회원가입" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "회원가입" });
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, emil, password, password2 },
+  } = req;
+  if (password !== password2) {
+    // 비밀번호가 일치하지 않을 때
+    res.status(400);
+    res.render("join", { pageTitle: "회원가입" });
+  } else {
+    // 비밀번호가 일치할 때
+    res.redirect(routes.home);
+  }
+};
+
 export const login = (req, res) => res.render("login", { pageTile: "로그인" });
 export const logout = (req, res) => res.send("Logout");
 export const users = (req, res) => res.send("Users");
