d6dd38d3fe140620569433ed634deed6cac494f1
haharimi <haharimi@naver.com>
2020-09-18 17:34:38 +0900

    Install mongoDB and moongose and Connecting DB

--- init.js
+++ init.js
@@ -1,6 +1,10 @@
 import app from "./app";
+import "./db";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+// env파일에서 PORT를 찾고 혹시 못 찾을 경우에는 3000번으로 연결
 
 function handleListening() {
   console.log(`🔔 Listening on: http://localhost:${PORT}`);
