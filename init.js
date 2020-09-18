c58b5990eb76e49a58097d4aae9ce357989a2b68
haharimi <haharimi@naver.com>
2020-09-18 17:59:00 +0900

    Making a Comment model and Connectiong Video.js to Comments.js

--- init.js
+++ init.js
@@ -2,6 +2,8 @@ import app from "./app";
 import "./db";
 import dotenv from "dotenv";
 dotenv.config();
+import "./model/Video";
+import "./model/Comment";
 
 const PORT = process.env.PORT || 3000;
 // env파일에서 PORT를 찾고 혹시 못 찾을 경우에는 3000번으로 연결
