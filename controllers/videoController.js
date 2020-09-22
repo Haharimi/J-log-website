42468a922c8dcbac36f93418e4cfd4f5dcae9d22
haharimi <haharimi@naver.com>
2020-09-22 15:54:21 +0900

    Install a Multer and Uploading Video to DB

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,15 @@
 import routes from "../routes";
+import Video from "../model/Video";
 
-export const home = (req, res) =>
-  res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos });
+  }
+};
 
 export const search = (req, res) => {
   const {
@@ -13,12 +21,18 @@ export const search = (req, res) => {
 export const getUpload = (req, res) => {
   res.render("upload", { pageTitle: "Upload" });
 };
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
-    body: { file, title, description },
+    body: { title, description },
+    file: { path },
   } = req;
-  // To do : upload and save videos
-  res.redirect(routes.videoDetail(121212));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description,
+  });
+  console.log(newVideo);
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) => {
