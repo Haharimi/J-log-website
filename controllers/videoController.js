12c42b22fac44cd931f63bd939c2767f48b2c166
haharimi <haharimi@naver.com>
2020-09-22 17:59:57 +0900

    MongoDB 작업 완료

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,7 +3,7 @@ import Video from "../model/Video";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ _id: -1 });
     res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     console.log(error);
