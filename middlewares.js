70efdb706da9c0bacd9650b6a8a1ba889a16b980
haharimi <haharimi@naver.com>
2020-10-03 11:41:21 +0900

    footer CSS 작업 완료

--- middlewares.js
+++ middlewares.js
@@ -1,16 +1,17 @@
-import multer from "multer";
-import routes from "./routes";
+import multer from 'multer';
+import routes from './routes';
+import { FALSE } from 'node-sass';
 
-const multerVideo = multer({ dest: "uploads/videos/" });
+const multerVideo = multer({ dest: 'uploads/videos/' });
 
 export const localsMiddleware = (rep, res, next) => {
-  res.locals.siteName = "J-Log";
+  res.locals.siteName = 'J-Log';
   res.locals.routes = routes;
   res.locals.user = {
-    isAuthenticated: true,
+    isAuthenticated: false,
     id: 1,
   };
   next();
 };
 
-export const uploadVideo = multerVideo.single("videoFile");
+export const uploadVideo = multerVideo.single('videoFile');
